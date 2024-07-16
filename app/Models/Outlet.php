26c27a582c16a8f68f0548868758a27889312562
mamanssuparman <63030102+mamanssuparman@users.noreply.github.com>
2024-07-16 20:59:02 +0700

    fix(outlet): add new column

--- app/Models/Outlet.php
+++ app/Models/Outlet.php
@@ -11,12 +11,14 @@ class Outlet extends Model
     use HasFactory;
     protected $fillable= [
         'ops_id',
+        'location_id',
         'name',
         'address',
         'phone',
         'email',
         'lat',
         'long',
+        'is_active',
         'type'
     ];
     protected $table = 'outlets';
