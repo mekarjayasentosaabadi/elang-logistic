d6a9d22af857c882653c89c71102e02dcde90311
mamanssuparman <63030102+mamanssuparman@users.noreply.github.com>
2024-08-30 23:43:57 +0700

    fix(customer): fixing remove delete menu user and filter outlet

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -47,10 +47,6 @@ class CustomerController extends Controller
                                 <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 me-50"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg>
                                 <span>Edit</span>
                         </a>
-                        <a class="dropdown-item"  href="' . url('/customer/' . $encryptId) . '" data-confirm-delete="true">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash me-50"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
-                            <span>Hapus</span>
-                        </a>
                     </div>
                 </div>';
                 return $btn;
@@ -74,7 +70,7 @@ class CustomerController extends Controller
      */
     public function create()
     {
-        $outlet = Outlet::where('is_active', '1')->get();
+        $outlet = Outlet::where('is_active', '1')->where('type', '!=', 2)->get();
         return view('pages.customer.create', compact('outlet'));
     }
 
@@ -161,7 +157,7 @@ class CustomerController extends Controller
     public function edit($id)
     {
         $customer   = User::where('id', Crypt::decrypt($id))->where('role_id', '4')->firstOrFail();
-        $outlet     = Outlet::where('is_active', '1')->get();
+        $outlet     = Outlet::where('is_active', '1')->where('type', '!=', 2)->get();
         return view('pages.customer.update', compact('customer', 'outlet'));
     }
 
