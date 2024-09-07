e699d7d11283b085685d7313be761a073796ba10
SonnySon1 <sonysudrajat94@gmail.com>
2024-09-08 06:37:51 +0700

    revisi masterprice

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,12 +16,12 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'John',
-        //     'email' => 'superadmin@elang.com',
-        //     'is_active' => 1,
-        //     'role_id' => '1',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'John',
+            'email' => 'superadmin@elang.com',
+            'is_active' => 1,
+            'role_id' => '1',
+        ]);
 
         // destination
         $data = [
@@ -67,53 +67,53 @@ class DatabaseSeeder extends Seeder
             ]);
         }
 
-        // // customer
-        // $data = [
-        //     [
-        //         'code_customer'  => 'C-001',
-        //         'name' => 'PT. ABC',
-        //         'email' => 'admin@abc.com',
-        //         'phone' => '08123456789',
-        //         'password' =>Hash::make('elang123'),
-        //         'address' => 'Jl. ABC No. 123',
-        //         'role_id' => '4',
-        //     ],
-        //     [
-        //         'code_customer'  => 'C-002',
-        //         'name' => 'PT. Alizwell',
-        //         'email' => 'admin@alizwell.id',
-        //         'phone' => '08123456789',
-        //         'password' =>Hash::make('elang123'),
-        //         'address' => 'Jl. Alizwell No. 123',
-        //         'role_id' => '4',
-        //     ],
-        // ];
-        // foreach ($data as $key => $value) {
-        //     DB::table('users')->insert($value);
-        // }
+        // customer
+        $data = [
+            [
+                'code_customer'  => 'C-001',
+                'name' => 'PT. ABC',
+                'email' => 'admin@abc.com',
+                'phone' => '08123456789',
+                'password' =>Hash::make('elang123'),
+                'address' => 'Jl. ABC No. 123',
+                'role_id' => '4',
+            ],
+            [
+                'code_customer'  => 'C-002',
+                'name' => 'PT. Alizwell',
+                'email' => 'admin@alizwell.id',
+                'phone' => '08123456789',
+                'password' =>Hash::make('elang123'),
+                'address' => 'Jl. Alizwell No. 123',
+                'role_id' => '4',
+            ],
+        ];
+        foreach ($data as $key => $value) {
+            DB::table('users')->insert($value);
+        }
 
 
-        // // customer_prices
-        // $outlet_id = [1, 2, 3];
-        // $destination_id = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34];
-        // $armada = [1, 2, 3];
-        // $estimation = [1, 2, 3, 4, 5, 6, 7];
-        // $price = [100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000];
-        // $data = [];
-        // foreach ($outlet_id as $outlet) {
-        // foreach ($destination_id as $destination) {
-        // foreach ($armada as $arm) {
-        // $data[] = [
-        // 'customer_id' => 1,
-        // 'outlet_id' => $outlet,
-        // 'destination_id' => $destination,
-        // 'armada' => $arm,
-        // 'price' => $price[array_rand($price)],
-        // 'estimation' => $estimation[array_rand($estimation)],
-        // ];
-        //         }
-        //     }
-        // }
-        // DB::table('customer_prices')->insert($data);
+        // customer_prices
+        $outlet_id = [1, 2, 3];
+        $destination_id = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34];
+        $armada = [1, 2, 3];
+        $estimation = [1, 2, 3, 4, 5, 6, 7];
+        $price = [100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000];
+        $data = [];
+        foreach ($outlet_id as $outlet) {
+        foreach ($destination_id as $destination) {
+        foreach ($armada as $arm) {
+        $data[] = [
+        'customer_id' => 1,
+        'outlet_id' => $outlet,
+        'destination_id' => $destination,
+        'armada' => $arm,
+        'price' => $price[array_rand($price)],
+        'estimation' => $estimation[array_rand($estimation)],
+        ];
+                }
+            }
+        }
+        DB::table('customer_prices')->insert($data);
     }
 }
