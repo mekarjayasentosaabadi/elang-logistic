fa65353225df2867743bd72365ba99ec9f28f4b5
SonnySon1 <sonysudrajat94@gmail.com>
2024-09-14 02:20:29 +0700

    menambahkan filter di order

--- resources/views/pages/order/index.blade.php
+++ resources/views/pages/order/index.blade.php
@@ -11,6 +11,64 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-header">
+                    <h4 class="card-title">Filter Transaksi</h4>
+                </div>
+                <div class="card-body">
+                    <div>
+                        <form action="" method="POST" id="form-filter-order">
+                            <div class="row mt-2" id="">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="status_order">Status Order</label>
+                                        <select name="status_order" id="status_order" class="form-control">
+                                            <option value="">Pilih Status Order</option>
+                                            <option value="1">Pending</option>
+                                            <option value="2">Process</option>
+                                            <option value="3">Done</option>
+                                            <option value="4">Dibatalkan</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="pengambilan">Pengambilan</label>
+                                        <select name="pengambilan" id="pengambilan" class="form-control">
+                                            <option value="">Pilih  Pengambilan</option>
+                                            @foreach ($destinations as $destination)
+                                                <option value="{{ $destination->id }}">{{ $destination->name }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row mt-2" id="">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="keterlambatan">Keterlambatan</label>
+                                        <select name="keterlambatan" id="keterlambatan" class="form-control">
+                                            <option value="">Pilih Keterlambatan</option>
+                                            <option value="Terlambat">Terlambat</option>
+                                            <option value="Hampir Terlambat">Hampir Terlambat</option>
+                                            <option value="Masih Dalam Estimasi">Masih Dalam Estimasi</option>
+                                            <option value="Tepat Waktu">Tepat Waktu</option>
+                                            <option value="Belum Ada Estimasi">Belum Ada Estimasi</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mt-2 text-end">
+                                <button type="submit" class="btn btn-primary"><i data-feather="eye" class="font-medium-3 me-50"></i>Lihat</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
                     <h4 class="card-title">Daftar Transaksi</h4>
                     @if (Auth::user()->role_id != '4')
                         <a href="{{ url('/order/create') }}" class="btn btn-primary"><li class="fa fa-plus"></li> Tambah Tansaksi</a>
@@ -47,6 +105,10 @@
 @section('custom-js')
     <script>
         $(document).ready(function() {
+
+            $('#pengambilan').select2();
+
+
             $('#tbl-order').DataTable({
                 processing: true,
                 serverSide: true,
@@ -112,6 +174,94 @@
                     }
                 ]
             });
+
+
+
+           $('#form-filter-order').on('submit', function(e) {
+               e.preventDefault();
+
+               var outlet_id = $('#outlet_id').val();
+               var destination_id = $('#destination_id').val();
+               var keterlambatan = $('#keterlambatan').val();
+               var status = $('#status').val();
+
+               $('#tbl-order').DataTable().destroy();
+
+               $('#tbl-order').DataTable({
+                   processing: true,
+                   serverSide: true,
+                   ajax: {
+                       url: "{{ url('/order/getAll') }}",
+                       type: 'GET',
+                       data: {
+                           pengambilan: $('#pengambilan').val(),
+                           status_order: $('#status_order').val(),
+                           keterlambatan: $('#keterlambatan').val(),
+                       },
+                   },
+                   columns: [{
+                           data: 'DT_RowIndex',
+                           orderable: false,
+                           searchable: false
+                       },
+                       {
+                           data: 'numberorders',
+                           name: 'orders.numberorders',
+                           orderable: true,
+                           searchable: true
+                       },
+                       {
+                           data: 'destination',
+                           name: 'destination.name',
+                           searchable: true
+                       },
+                       {
+                           data: 'pengirim',
+                           name: 'customer.name',
+                           orderable: true,
+                           searchable: true
+                       },
+                       {
+                           data: 'penerima',
+                           name: 'orders.penerima',
+                           orderable: true,
+                           searchable: true
+                       },
+                       {
+                           data: 'created_at',
+                           name: 'orders.created_at',
+                           orderable: true,
+                           searchable: true
+                       },
+                       {
+                           data: 'note',
+                           name: 'note',
+                       },
+                       {
+                           data: 'status_orders',
+                           name: 'status_orders',
+                           orderable: true,
+                           searchable: true
+                       },
+                       {
+                           data: 'status_kenerlambatan',
+                           name: 'status_kenerlambatan',
+                           orderable: false,
+                           searchable: false
+                       },
+                       {
+                           data: 'aksi',
+                           name: 'aksi',
+                           orderable: false,
+                           searchable: false
+                       }
+                   ]
+               });
+
+           });
+
+
+
         });
     </script>
 @endsection
